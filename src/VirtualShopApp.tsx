5129251481b633248b308a99c17e1c6d7e10ca45
angel99gc <angel.gamboa12@hotmail.com>
2025-08-29 23:12:45 -0600

    Update 2 - Base de react con vite, shadcdn, router, tailwindCSS

---
+++ src/VirtualShopApp.tsx
@@ -0,0 +1,11 @@
+
+import { RouterProvider } from 'react-router'
+import { appRouter } from './app.router.tsx'
+
+function VirtualShopApp() {
+
+  return <RouterProvider router={appRouter} />
+
+}
+
+export default VirtualShopApp
---
+++ src/shop/pages/home/HomePage.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button"
+
+export const HomePage = () => {
+  return (
+    <div>
+      HomePage
+      <h1 className="font-montserrat">Hola</h1>
+      <h1 className="font-montserrat font-thin">Hola</h1>
+      <h1 className="font-montserrat font-normal">Hola</h1>
+      <h1 className="font-montserrat font-bold">Hola</h1>
+      <Button>Button</Button>
+
+
+      <p>Welcome to the HomePage!</p>
+    </div>
+  )
+}
