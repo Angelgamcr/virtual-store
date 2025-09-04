015acfaa41ae4717c13b8525d4183031c6fbb6e3
angel99gc <angel.gamboa12@hotmail.com>
2025-09-03 23:36:56 -0600

    Uso de react-hook-form para manejo de los datos digitados por el usuario - Se crea conexion para obtener por id, actualizar y crear el producto usando el tanstack para que mientras se envia la peticion al api se muestre la pantalla de carga, ademas de guardar en cache los datos obtenidos que no requieren cambios

---
+++ src/admin/hooks/useProduct.tsx
@@ -0,0 +1,40 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getProductByIdAction } from "../action/get-product-by-id.action";
+import type { Product } from "@/interfaces/product.interface";
+import { createUpdateProductAction } from "../action/create-update-product.action";
+
+export const useProduct = (id: string) => {
+
+  const queryClient = useQueryClient();// Brinda toda la informacion para modificar el query
+
+  const query = useQuery({
+    queryKey: ['product', { id }],
+    queryFn: () => getProductByIdAction(id),
+    retry: false,
+    staleTime: 1000 * 60 * 5, // 5 minutes,
+    // enabled: !!id
+  })
+
+
+  const mutation = useMutation({
+    mutationFn: createUpdateProductAction,
+    onSuccess: (product: Product) => {
+
+      // Invalidar cache
+      queryClient.invalidateQueries({ queryKey: ['products'] })
+      queryClient.invalidateQueries({
+        queryKey: ['product', { id: product.id }]
+      })
+
+      // Actualizar queryData
+      queryClient.setQueryData(['products', { id: product.id }], product)
+
+    }
+  });
+
+
+  return {
+    ...query,
+    mutation
+  };
+}
