55838d69fd81f863d78cac99e2ca99b59cce133d
angel99gc <angel.gamboa12@hotmail.com>
2025-09-18 22:51:27 -0600

    Ajustes para instanciacion

--- src/lib/hooks/useLanguage.tsx
+++ src/lib/hooks/useLanguage.tsx
@@ -1,11 +1,11 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import { translations } from '../constants/translations';
 
 // TODO: FALTA Optimizar para cambio de idioma
 
 const LanguageContext = createContext({
   language: 'es',
-  setLanguage: (lng: string) => { },
+  setLanguage: (lng: string) => { console.log(lng) },
   t: {},
 });
 export const LanguageProvider = ({ children }: { children: React.ReactNode }) => {
@@ -39,10 +39,10 @@ export const LanguageProvider = ({ children }: { children: React.ReactNode }) =>
   );
 }
 
-export const useLanguage = () => {
-  const context = useContext(LanguageContext);
-  if (context === undefined) {
-    throw new Error('useLanguage must be used within a LanguageProvider');
-  }
-  return context;
-}
+// export const useLanguage = () => {
+//   const context = useContext(LanguageContext);
+//   if (context === undefined) {
+//     throw new Error('useLanguage must be used within a LanguageProvider');
+//   }
+//   return context;
+// }
